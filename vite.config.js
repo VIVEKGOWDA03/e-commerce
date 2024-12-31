10a7d2af9390c4f381e2c9ab99f693e6bc48c787
vivek <vivekck12343@gmail.com>
2024-12-31 23:29:24 +0530

    Fix process is not defined error

--- vite.config.js
+++ vite.config.js
@@ -1,14 +1,17 @@
-import { defineConfig } from 'vite';
-import path from 'path';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from "vite";
+import path from "path";
+import react from "@vitejs/plugin-react";
 
+// Vite configuration
 export default defineConfig({
-  plugins: [
-    react(),
-  ],
+  plugins: [react()],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
     },
+  },
+  define: {
+    // Define `process.env` as an empty object to avoid errors
+    "process.env": {},
   },
 });
