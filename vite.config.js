bb2dafe0992a24b0c7ff728dd00a487a5b327cc3
vivek <vivekck12343@gmail.com>
2024-12-31 22:58:54 +0530

    Fix process is not defined error

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,16 @@
-import { defineConfig } from 'vite'
-import path from "path"
-import react from '@vitejs/plugin-react'
-import NodeGlobalsPolyfillPlugin from 'vite-plugin-node-polyfills';
-// https://vite.dev/config/
+import { defineConfig } from 'vite';
+import path from 'path';
+import react from '@vitejs/plugin-react';
+import { NodeGlobalsPolyfillPlugin } from 'vite-plugin-node-polyfills'; // Named import
+
 export default defineConfig({
-  plugins: [react(),NodeGlobalsPolyfillPlugin()],
+  plugins: [
+    react(),
+    NodeGlobalsPolyfillPlugin() // Apply the polyfill plugin correctly
+  ],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
     },
   },
-})
+});
